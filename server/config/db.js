7a7e1a7d302b52e0ce5ec7acb1139a6ab2e674c5
maesterfox <talentedmrfox@hotmail.com>
2023-12-30 04:22:47 +0000

    updating routes, connected to MongoDB, add/remove buttons

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const connectDB = async () => {
-  
   try {
-    mongoose.set('strictQuery', false);
+    mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    process.exit(1); // Exit the process with failure
   }
-
-}
+};
 
 module.exports = connectDB;
